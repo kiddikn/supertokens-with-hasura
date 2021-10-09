ccf37f33abd583e5e22ca22c4e67b22dbe7bf1a6
kiddikn <kiddikn@gmail.com>
2021-10-09 15:26:13 +0900

    add log

--- main.go
+++ main.go
@@ -152,8 +152,11 @@ func httpServer(httpPort int, webSiteDomain, hasuraEndPoint string) (*http.Serve
 
 func corsMiddleware(next http.Handler, webSiteDomain, hasuraEndPoint string) http.Handler {
 	allowHost := func(host string) string {
+		origin := fmt.Sprintf("https://%s", host)
+		fmt.Println(origin)
 		for _, a := range []string{webSiteDomain, hasuraEndPoint} {
-			if fmt.Sprintf("https://%s", host) == a {
+			fmt.Println(a)
+			if origin == a {
 				return host
 			}
 		}

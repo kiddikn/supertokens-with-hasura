d8b5fa2da50b78dd485a6be4dfc7a9f1c149d13b
kiddikn <kiddikn@gmail.com>
2021-10-09 15:23:40 +0900

    add schema to check host

--- main.go
+++ main.go
@@ -152,9 +152,8 @@ func httpServer(httpPort int, webSiteDomain, hasuraEndPoint string) (*http.Serve
 
 func corsMiddleware(next http.Handler, webSiteDomain, hasuraEndPoint string) http.Handler {
 	allowHost := func(host string) string {
-		fmt.Println(host)
 		for _, a := range []string{webSiteDomain, hasuraEndPoint} {
-			if host == a {
+			if fmt.Sprintf("https://%s", host) == a {
 				return host
 			}
 		}
